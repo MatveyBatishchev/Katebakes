35bbcbdb1f2bdc0f02607b1ab30e75b311111e65
Matvey Batishchev <batishevmatvey@mail.ru>
2023-02-28 19:31:20 +0300

    Initial commit

---
+++ src/test/java/com/example/katebakes/KatebakesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.katebakes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KatebakesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
